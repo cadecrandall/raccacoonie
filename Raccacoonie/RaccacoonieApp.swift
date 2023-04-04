7d4e43ca64c15a57fad4ff7db4d3481cf3c09697
Cade Crandall <cadecranda@gmail.com>
2023-04-04 13:18:53 -0600

    Initial Commit

---
+++ Raccacoonie/RaccacoonieApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaccacoonieApp.swift
+//  Raccacoonie
+//
+//  Created by Cade Crandall on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RaccacoonieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
