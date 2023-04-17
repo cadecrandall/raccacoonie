d291238cf32261cd43e91cbc1e774beed79f8c66
Cade Crandall <cadecranda@gmail.com>
2023-04-16 21:15:39 -0600

    authorization is working but cant get the right track to show up

--- Raccacoonie/Data/SpotifyFetcher.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SpotifyFetcher.swift
-//  Raccacoonie
-//
-//  Created by Cade Crandall on 4/10/23.
-//
-
-import Foundation
---
+++ Raccacoonie/Data/Track.swift
@@ -0,0 +1,16 @@
+//
+//  Track.swift
+//  Raccacoonie
+//
+//  Created by Cade Crandall on 4/16/23.
+//
+
+import Foundation
+
+struct CCCTrack {
+    let name: String
+    let artist: String
+    let uri: String?
+    let id: String?
+    
+}
